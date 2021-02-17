9908eb12a6d45b060bba1da68fc94357655d4a22
phaalonso <26033899+phaalonso@users.noreply.github.com>
2021-02-17 11:31:03 -0300

    Permitindo cadastrar multiplas imagens

--- frontend/src/app/criar-feedback/criar-feedback.page.ts
+++ frontend/src/app/criar-feedback/criar-feedback.page.ts
@@ -1,8 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ModalController, ToastController } from '@ionic/angular';
 import { ApiService, Feedback } from '../services/api.service';
-import { PhotoService } from '../services/photo.service';
+import { Photo, PhotoService } from '../services/photo.service';
 
 @Component({
   selector: 'app-criar-feedback',
@@ -10,10 +10,9 @@ import { PhotoService } from '../services/photo.service';
   styleUrls: ['./criar-feedback.page.scss'],
 })
 export class CriarFeedbackPage implements OnInit {
-
   @Input() feedbacks: Feedback[];
   public formulario: FormGroup;
-  public imgForm: FormGroup;
+  public images: FormArray;
 
   constructor(
     public modalController: ModalController,
@@ -26,11 +25,22 @@ export class CriarFeedbackPage implements OnInit {
       titulo: ['', [Validators.required, Validators.minLength(3)]],
       mensagem: ['', [Validators.required, Validators.minLength(3)]],
       tipo: [0, [Validators.required, Validators.min(0), Validators.max(2)]],
+      images: this.formBuilder.array([]),
     });
+  }
 
-    this.imgForm = this.formBuilder.group({
-      url: ['', Validators.required]
+  createImg(url: string): FormGroup {
+    return this.formBuilder.group({
+      url: [url, Validators.required],
     });
+  }
+
+  addPhoto(photo: Photo) {
+    this.images = this.formulario.get('images') as FormArray;
+    console.log(this.images);
+    this.images.push(this.createImg(photo.webViewPath));
+
+    console.log(this.images);
   }
 
   async toast(message: string) {
@@ -45,26 +55,22 @@ export class CriarFeedbackPage implements OnInit {
 
   async cadastrar() {
     if (this.formulario.valid) {
-      this.feedbacks.unshift(this.formulario.value);
-
       const feedback: Feedback = {
         ...this.formulario.value,
-        images: [this.imgForm.value],
-        usuario: 1
+        usuario: 1,
       };
 
       console.log('Feedback', feedback);
       const response = await this.apiService.uploadPostagem(feedback);
 
       if (!response) {
-        this.toast('Ocorreu um problema ao cadastrar')
+        this.toast('Ocorreu um problema ao cadastrar');
       }
 
       this.dismiss();
     } else {
       this.toast('Preencha corretamente os campos');
       this.formulario.markAllAsTouched();
-      this.imgForm.markAllAsTouched();
     }
   }
 
@@ -73,18 +79,16 @@ export class CriarFeedbackPage implements OnInit {
   }
 
   async adicionarFoto() {
+    console.log('Adicionar foto');
     await this.photoService.addNewToGallery();
-    const photo = this.photoService.photos[0];
-    // console.log('Photo', photo);
 
-    this.imgForm.controls.url.setValue(photo.webViewPath);
-    // console.log(this.imgForm.get('url').value);
+    const photo = this.photoService.photos[this.images?.length || 0]
+    console.log('Photo', photo);
+
+    this.addPhoto(photo);
 
     // console.log(this.photoService.photos);
-
   }
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
